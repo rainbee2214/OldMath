b860299380893170c0ec6a0a51e1535ccff4244f
Sarah MacDonald <rainbee2214@gmail.com>
2014-04-19 00:03:44 -0300

    Added division and remainders

--- MathPG/Assets/Scripts/PlayerController.cs
+++ MathPG/Assets/Scripts/PlayerController.cs
@@ -18,7 +18,7 @@ public class PlayerController : MonoBehaviour
 	private bool multiply = false;
 	private bool divide = false;
 
-	public GameObject temp1, temp2;
+	private GameObject temp1, temp2, temp3;
 
 
 	private Vector2 direction;
@@ -62,7 +62,7 @@ public class PlayerController : MonoBehaviour
 		}
 	}
 
-	int showProduct(int self, int nextNumber)
+	void showProduct(int self, int nextNumber)
 	{
 		int product = self * nextNumber;
 		direction = transform.position;
@@ -70,17 +70,26 @@ public class PlayerController : MonoBehaviour
 		direction.x += 0.5f;
 		temp1 = Instantiate(prefabs[product % 10], direction, Quaternion.identity) as GameObject;
 		direction.x += -1;
-		//Debug.Log("Product: " + product);
-		//Debug.Log("Product%10: " + (product % 10));
-		//Debug.Log("Product/10: " + (product / 10));
-		temp2 = Instantiate(prefabs[product / 10], direction, Quaternion.identity) as GameObject;
-		return product;
+		if (product / 10 != 0) temp2 = Instantiate(prefabs[product / 10], direction, Quaternion.identity) as GameObject;
+
 	}
 
-	int showDividend(int self, int nextNumber)
+	void showDividend(int self, int nextNumber)
 	{
-		int dividend = 0;
-		return dividend;
+		int dividend = self / nextNumber;
+		int mod = self % nextNumber;
+		direction = transform.position;
+		direction.y += 1;
+		direction.x += 0.5f;
+		temp1 = Instantiate(prefabs[10], direction, Quaternion.identity) as GameObject;
+		direction.x += -1;
+		temp2 = Instantiate(prefabs[dividend], direction, Quaternion.identity) as GameObject;
+		direction.x += 2f;
+		temp3 = Instantiate(prefabs[mod], direction, Quaternion.identity) as GameObject;
+		temp3.transform.localScale = new Vector3(.75f, .75f, 1);
+		Debug.Log(dividend);
+		Debug.Log(dividend/10);
+		Debug.Log(dividend%10);
 	}
 
 	void OnTriggerEnter2D (Collider2D other)
@@ -113,14 +122,13 @@ public class PlayerController : MonoBehaviour
 				}
 				if (divide) 
 				{
-					self /= nextNumber;
-					//showDividend(self, nextNumber);
+					showDividend(GameController.gameController.CurrentNumber, nextNumber);
+					int temp = self % 10;
+					self = temp / nextNumber;
 				}
 				
 				if (self < 0) self = 100 - Mathf.Abs(self);
 				if (self > 99) self = self - 100;
-				//Debug.Log("Self: " + self);
-				//Debug.Log("Current number: " + GameController.gameController.CurrentNumber);
 				GameController.gameController.CurrentNumber = self % 10;
 			}
 			
