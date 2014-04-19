c20dbdb97270c20a92646534a5158039a0abd731
Sarah MacDonald <rainbee2214@gmail.com>
2014-04-19 01:10:13 -0300

    Battle scenes working, still lots of errors

---
+++ MathPG/Assets/Scripts/Timer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour 
+{
+	public static float battleLength = 5;
+	private float targetTime;
+	void Start () 
+	{
+		targetTime = battleLength + Time.time;
+		//Debug.Log(battleLength);
+		//Debug.Log(Time.time);
+	}
+	
+
+	void Update () 
+	{
+		//Debug.Log(Time.time);
+		if (Time.time > targetTime) Application.LoadLevel("TitleScreen");
+	}
+}
---
+++ MathPG/Assets/SpriteController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteController : MonoBehaviour 
+{
+	public bool hasCollided = false;
+
+	void Update () 
+	{
+		Debug.Log(Application.loadedLevelName);
+		//if (hasCollided) Destroy(this.gameObject);
+		if (Application.loadedLevelName != "TitleScreen")
+		{
+			renderer.enabled = false;
+		}
+		else
+		{
+			renderer.enabled = true;
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		hasCollided = true;
+	}
+}
