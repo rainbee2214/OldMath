839fd7d7f834a5ab8618cb9261664a6b95a08d88
Sarah MacDonald <rainbee2214@gmail.com>
2014-04-18 23:02:05 -0300

    Fixed several problems

---
+++ MathPG/Assets/EdgeCollider.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EdgeCollider : MonoBehaviour 
+{
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		Debug.Log("Trigger exit");
+		if (other.tag == "Show") Destroy(other.gameObject);
+	}
+}
--- MathPG/Assets/Scripts/PlayerController.cs
+++ MathPG/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
 	public GameObject[] prefabs;
 	public Sprite[] sprites;
+
 	private SpriteRenderer renderer;
 	private string lastCollision;
 	private Vector2 movement;
@@ -17,14 +18,25 @@ public class PlayerController : MonoBehaviour
 	private bool multiply = false;
 	private bool divide = false;
 
+	public GameObject temp1, temp2;
+
+
+	private Vector2 direction;
 	void Start () 
 	{
 		renderer = gameObject.GetComponent<SpriteRenderer>();
 		renderer.sprite = sprites[GameController.gameController.CurrentNumber];
+
+
+
+
 	}
 
 	void Update()
 	{
+		//Debug.Log("Current Number: " + GameController.gameController.CurrentNumber);
+		//Debug.Log("Self: " + self);
+
 		renderer.sprite = sprites[GameController.gameController.CurrentNumber];
 	}
 
@@ -50,35 +62,70 @@ public class PlayerController : MonoBehaviour
 		}
 	}
 
+	int showProduct(int self, int nextNumber)
+	{
+		int product = self * nextNumber;
+		direction = transform.position;
+		direction.y += 1;
+		direction.x += 0.5f;
+		temp1 = Instantiate(prefabs[product % 10], direction, Quaternion.identity) as GameObject;
+		direction.x += -1;
+		//Debug.Log("Product: " + product);
+		//Debug.Log("Product%10: " + (product % 10));
+		//Debug.Log("Product/10: " + (product / 10));
+		temp2 = Instantiate(prefabs[product / 10], direction, Quaternion.identity) as GameObject;
+		return product;
+	}
+
+	int showDividend(int self, int nextNumber)
+	{
+		int dividend = 0;
+		return dividend;
+	}
+
 	void OnTriggerEnter2D (Collider2D other)
 	{
-		self = GameController.gameController.CurrentNumber;
-
-
-		// If it's an operator
-		if (other.tag == "+" || other.tag == "-" || other.tag == "multiply" || other.tag == "divide") 
+		if (other.tag != "Show")
 		{
-			changeOperator(other.tag);
-			lastCollision = other.tag;
+			// If it's an operator
+			if (other.tag == "+" || other.tag == "-" || other.tag == "multiply" || other.tag == "divide") 
+			{
+				changeOperator(other.tag);
+				lastCollision = other.tag;
+				
+			}
+			
+			// If it's a number
+			if (other.tag == "1" || other.tag == "2" || other.tag == "3" ||
+			    other.tag == "4" || other.tag == "5" || other.tag == "6" ||
+			    other.tag == "7" || other.tag == "8" || other.tag == "9" ||
+			    other.tag == "0")
+			{
+				nextNumber = int.Parse(other.tag);
+				if (add) self += nextNumber;
+				if (subtract) self -= nextNumber;
+				if (multiply) 
+				{
+					showProduct(GameController.gameController.CurrentNumber, nextNumber);
+					int temp = self % 10;
+					self = temp * nextNumber;
+					
+				}
+				if (divide) 
+				{
+					self /= nextNumber;
+					//showDividend(self, nextNumber);
+				}
+				
+				if (self < 0) self = 100 - Mathf.Abs(self);
+				if (self > 99) self = self - 100;
+				//Debug.Log("Self: " + self);
+				//Debug.Log("Current number: " + GameController.gameController.CurrentNumber);
+				GameController.gameController.CurrentNumber = self % 10;
+			}
+			
+			if(other.tag != "Edge") Destroy(other.gameObject);
 
 		}
-
-		// If it's a number
-		if (other.tag == "1" || other.tag == "2" || other.tag == "3" ||
-		    other.tag == "4" || other.tag == "5" || other.tag == "6" ||
-		    other.tag == "7" || other.tag == "8" || other.tag == "9" ||
-		    other.tag == "0")
-		{
-			nextNumber = int.Parse(other.tag);
-			if (add) self += nextNumber;
-			if (subtract) self -= nextNumber;
-			if (multiply) self *= nextNumber;
-			if (divide) self /= nextNumber;
-			//Debug.Log(self);
-			if (self < 0) self = 100 - Mathf.Abs(self);
-			GameController.gameController.CurrentNumber = self % 10;
-		}
-		Destroy(other.gameObject);
-
 	}
 }
