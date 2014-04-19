1daaf7cc550f7febec94ff50c7dec3b7dca5b645
Sarah MacDonald <rainbee2214@gmail.com>
2014-04-18 21:34:39 -0300

    Added sprites and prefabs for main numbers etc

---
+++ MathPG/Assets/Scripts/GameController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour 
+{
+	public static GameController gameController;
+
+
+
+
+	#region Properties
+	private int score = 0;
+	public int Score
+	{
+		get{return score;}
+		set{score += value;}
+	}
+	
+	private int currentNumber = 0;
+	public int CurrentNumber
+	{
+		get{return currentNumber;}
+		set{currentNumber = value;}
+	}
+	
+	private bool playerDead = false;
+	public bool PlayerDead
+	{
+		get{return playerDead;}
+		set{playerDead = value;}
+	}
+
+	private float speed = 5;
+	public float Speed 
+	{
+		get{return speed;}
+		set{speed = value;}
+	}
+	#endregion
+
+	void Start () 
+	{
+
+	}
+
+	void Awake () 
+	{
+		//if control is not set, set it to this one and allow it to persist
+		if (gameController == null)
+		{
+			DontDestroyOnLoad(gameObject);
+			gameController = this;
+		}
+		//else if control exists and it isn't this instance, destroy this instance
+		else if(gameController != this)
+		{
+			Debug.Log ("Game control already exists, deleting this new one");
+			Destroy (gameObject);
+		}
+	}
+
+	void Update () 
+	{
+
+	}
+}
---
+++ MathPG/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour 
+{
+	public GameObject[] prefabs;
+	public Sprite[] sprites;
+	private SpriteRenderer renderer;
+	private string lastCollision;
+	private Vector2 movement;
+
+	private int self;
+	private int nextNumber;
+	private bool add = false;
+	private bool subtract = false;
+	private bool multiply = false;
+	private bool divide = false;
+
+	void Start () 
+	{
+		renderer = gameObject.GetComponent<SpriteRenderer>();
+		renderer.sprite = sprites[GameController.gameController.CurrentNumber];
+	}
+
+	void Update()
+	{
+		renderer.sprite = sprites[GameController.gameController.CurrentNumber];
+	}
+
+	void FixedUpdate () 
+	{
+		movement.x = Input.GetAxis ("Horizontal") * GameController.gameController.Speed;
+		movement.y = Input.GetAxis ("Vertical") * GameController.gameController.Speed;
+		rigidbody2D.velocity = movement;
+	}
+
+	void changeOperator(string operatorName)
+	{
+		add = false;
+		subtract = false;
+		multiply = false;
+		divide = false;
+		switch(operatorName)
+		{
+			case "+": add = true; break;
+			case "-": subtract = true; break;
+			case "multiply": multiply = true; break;
+			case "divide": divide = true; break;
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		self = GameController.gameController.CurrentNumber;
+
+
+		// If it's an operator
+		if (other.tag == "+" || other.tag == "-" || other.tag == "multiply" || other.tag == "divide") 
+		{
+			changeOperator(other.tag);
+			lastCollision = other.tag;
+
+		}
+
+		// If it's a number
+		if (other.tag == "1" || other.tag == "2" || other.tag == "3" ||
+		    other.tag == "4" || other.tag == "5" || other.tag == "6" ||
+		    other.tag == "7" || other.tag == "8" || other.tag == "9" ||
+		    other.tag == "0")
+		{
+			nextNumber = int.Parse(other.tag);
+			if (add) self += nextNumber;
+			if (subtract) self -= nextNumber;
+			if (multiply) self *= nextNumber;
+			if (divide) self /= nextNumber;
+			//Debug.Log(self);
+			if (self < 0) self = 100 - Mathf.Abs(self);
+			GameController.gameController.CurrentNumber = self % 10;
+		}
+		Destroy(other.gameObject);
+
+	}
+}
