2692fb75b0dc14edad0f5cdccdd470c26f8e38f0
Sarah MacDonald <rainbee2214@gmail.com>
2014-04-22 17:42:55 -0300

    Added a menu scene

---
+++ MathPG/Assets/MenuController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour 
+{
+	public GUIText titleNameText, option1Text, option2Text, option3Text;
+
+	void Start () 
+	{
+		titleNameText.text = "MathPG";
+		option1Text.text = "Start Game";
+		option2Text.text = "Settings";
+		option3Text.text = "Quit";
+	}
+
+	void Update () 
+	{
+	
+	}
+}
