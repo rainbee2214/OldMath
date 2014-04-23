eaefb10df29bb9159a1e1f2c1e8a07383b33841e
Sarah MacDonald <rainbee2214@gmail.com>
2014-04-23 16:36:35 -0300

    Centering vertically is working now

--- MathPG/Assets/Scripts/MenuController.cs
+++ MathPG/Assets/Scripts/MenuController.cs
@@ -21,6 +21,8 @@ public class MenuController : MonoBehaviour
 	private const float START_LOCATION_X = 0f;
 	private const float START_LOCATION_Y = 0.9f;
 	private const float	DISTANCE_BETWEEN = DISTANCE_BETWEEN_GUI * 2;
+	private float adjustedGUIStartY = START_GUI_LOCATION_Y;
+	private float adjustedStartY = START_LOCATION_Y;
 
 	// Temporary positions for placing everything on screen
 	//private float xPosition, yPosition;
@@ -30,13 +32,13 @@ public class MenuController : MonoBehaviour
 	private Vector3 scale;
 
 
-	public bool centeredH, bottomJustified;
+	public bool centeredH;
+	public int centerHeight;
 
 	// Starting location is the top middle of the screen by default. 10 options fit on the screen
 
 	private GUIText[] optionsGUIText;
 
-	private float startingLocation, startingGUILocation;
 
 	// Button
 	private GameObject[] buttons;
@@ -45,18 +47,77 @@ public class MenuController : MonoBehaviour
 	private int maxButtons;
 	private float buttonWidth = 1;
 
+	void setStartingPositions()
+	{
+		if (leftJustified)
+		{
+			buttonLocation.x = -1.4f;
+			guiLocation.x = 0.4f;
+		}
+		else if (rightJustified)
+		{
+			buttonLocation.x = 1.4f;
+			guiLocation.x = 0.6f;
+		} 
+		else
+		{
+			buttonLocation.x = START_LOCATION_X;
+			guiLocation.x = START_GUI_LOCATION_X;
+		}
 
+		if (centeredH)
+		{
+			setCenterHeight();
+			adjustedGUIStartY = guiLocation.y;
+			adjustedStartY = buttonLocation.y;
+		}
+	}
+
+	void setCenterHeight()
+	{
+		switch (maxButtons)
+		{
+		case 1: 
+			buttonLocation.y = -0.1f; 
+			guiLocation.y = 0.45f;
+			break;
+		case 2: 
+		case 3: 
+			buttonLocation.y = 0.1f; 
+			guiLocation.y = 0.55f;
+			break;
+		case 4: 
+		case 5: 
+			buttonLocation.y = 0.3f; 
+			guiLocation.y = 0.65f;
+			break;
+		case 6: 
+		case 7: 
+			buttonLocation.y = 0.5f; 
+			guiLocation.y = 0.75f;
+			break;
+		case 8:
+		case 9: 
+			buttonLocation.y = 0.7f; 
+			guiLocation.y = 0.85f;
+			break;
+		case 10: 
+			buttonLocation.y = 0.9f; 
+			guiLocation.y = 0.95f;
+			break;
+		}
+	}
 
 	void Start () 
 	{
-		selectorLocation = buttonLocation;
-
-		cameraSize = camera.camera.orthographicSize;
 		maxButtons = optionsText.Length;
+		setStartingPositions();
+
+		selectorLocation = buttonLocation;
+		cameraSize = camera.camera.orthographicSize;
+
 		buttons = new GameObject[maxButtons];
 		optionsGUIText = new GUIText[maxButtons];
-
-
 
 		scale = new Vector3(1,cameraSize * 0.15f,1);
 	
@@ -64,8 +125,8 @@ public class MenuController : MonoBehaviour
 		for (int i = 0; i < maxButtons; i++)
 		{
 			// Create GUI Text. 
-			guiLocation.y =  START_GUI_LOCATION_Y + DISTANCE_BETWEEN_GUI * i;
-			Debug.Log(guiLocation.x + "," + guiLocation.y);
+			guiLocation.y =  adjustedGUIStartY + DISTANCE_BETWEEN_GUI * i;
+	
 			optionsGUIText[i] = Instantiate(guiText, guiLocation, Quaternion.identity) as GUIText;
 			optionsGUIText[i].text = optionsText[i];
 			optionsGUIText[i].name = ("Text" + i);
@@ -75,7 +136,7 @@ public class MenuController : MonoBehaviour
 		for (int i = 0; i < maxButtons; i++)
 		{
 			// Create "Buttons"
-			buttonLocation.y = START_LOCATION_Y + DISTANCE_BETWEEN * i;
+			buttonLocation.y = adjustedStartY + DISTANCE_BETWEEN * i;
 			buttons[i] = Instantiate(button, buttonLocation * cameraSize, Quaternion.identity) as GameObject;
 			buttonWidth = optionsText[i].Length;
 			scale.x = (buttonWidth * cameraSize) / 10;
