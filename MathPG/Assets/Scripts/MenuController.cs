c1f47ae7d915a6947a251ce2173517e90f902066
Sarah MacDonald <rainbee2214@gmail.com>
2014-04-23 16:14:00 -0300

    Made a lot of changes to clean it up

--- MathPG/Assets/Scripts/MenuController.cs
+++ MathPG/Assets/Scripts/MenuController.cs
@@ -13,18 +13,30 @@ public class MenuController : MonoBehaviour
 	// Everything is scaled to the camera size
 	private float cameraSize;
 
+
+
+	private const float START_GUI_LOCATION_X = 0.5f;
+	private const float START_GUI_LOCATION_Y = 0.95f;
+	private const float DISTANCE_BETWEEN_GUI = -0.1f;
+	private const float START_LOCATION_X = 0f;
+	private const float START_LOCATION_Y = 0.9f;
+	private const float	DISTANCE_BETWEEN = DISTANCE_BETWEEN_GUI * 2;
+
 	// Temporary positions for placing everything on screen
-	private float xPosition, yPosition;
-	private Vector3 location;
+	//private float xPosition, yPosition;
+	private Vector3 selectorLocation;
+	private Vector3 buttonLocation = new Vector3(START_LOCATION_X,START_LOCATION_Y,1);
+	private Vector3 guiLocation = new Vector3(START_GUI_LOCATION_X,START_GUI_LOCATION_Y,1);
 	private Vector3 scale;
 
+
+	public bool centeredH, bottomJustified;
+
 	// Starting location is the top middle of the screen by default. 10 options fit on the screen
-	private float 	startingGUILocation = 0.95f, distanceGUIBetween = -0.1f, 
-					startingLocation = 0.9f,distanceBetween = -0.2f;
 
 	private GUIText[] optionsGUIText;
 
-
+	private float startingLocation, startingGUILocation;
 
 	// Button
 	private GameObject[] buttons;
@@ -33,67 +45,46 @@ public class MenuController : MonoBehaviour
 	private int maxButtons;
 	private float buttonWidth = 1;
 
+
+
 	void Start () 
 	{
+		selectorLocation = buttonLocation;
+
 		cameraSize = camera.camera.orthographicSize;
 		maxButtons = optionsText.Length;
 		buttons = new GameObject[maxButtons];
 		optionsGUIText = new GUIText[maxButtons];
 
-		location = new Vector3(xPosition,yPosition,1);
+
+
 		scale = new Vector3(1,cameraSize * 0.15f,1);
 	
+		//setPosition("GUIText");
 		for (int i = 0; i < maxButtons; i++)
 		{
-			// Create GUI Text. Set
-			setPositions("GUIText");
-			location.x = xPosition;
-			location.y = startingGUILocation + i*distanceGUIBetween;
-			optionsGUIText[i] = Instantiate(guiText, location, Quaternion.identity) as GUIText;
+			// Create GUI Text. 
+			guiLocation.y =  START_GUI_LOCATION_Y + DISTANCE_BETWEEN_GUI * i;
+			Debug.Log(guiLocation.x + "," + guiLocation.y);
+			optionsGUIText[i] = Instantiate(guiText, guiLocation, Quaternion.identity) as GUIText;
 			optionsGUIText[i].text = optionsText[i];
 			optionsGUIText[i].name = ("Text" + i);
 		}
+
+		//setPosition("Button");
 		for (int i = 0; i < maxButtons; i++)
 		{
-			setPositions("Button");
 			// Create "Buttons"
-			location.x = xPosition;
-			location.y = startingLocation + distanceBetween * i;
-			buttons[i] = Instantiate(button, location * cameraSize, Quaternion.identity) as GameObject;
+			buttonLocation.y = START_LOCATION_Y + DISTANCE_BETWEEN * i;
+			buttons[i] = Instantiate(button, buttonLocation * cameraSize, Quaternion.identity) as GameObject;
 			buttonWidth = optionsText[i].Length;
 			scale.x = (buttonWidth * cameraSize) / 10;
 			buttons[i].gameObject.transform.localScale = scale;
 			buttons[i].name = ("Button" + i);
 		}
-
-		setPositions("Selector");
 	}
 
-	
-	void setPositions(string positionTypeWanted)
-	{
-		if (positionTypeWanted == "Button")
-		{
-			if (leftJustified) xPosition = -1.4f;
-			else if (rightJustified) xPosition = 1.4f;	
-			else xPosition = 0f;
-			
-		}
-		else if (positionTypeWanted == "GUIText")
-		{
-			if (leftJustified) xPosition = 0.1f;
-			else if (rightJustified) xPosition = 0.9f;		
-			else xPosition = 0.5f;
-			
-		}
-		else if (positionTypeWanted == "Selector")
-		{
-			if (leftJustified) xPosition = -1.4f;
-			else if (rightJustified) xPosition = 1.4f;	
-			else xPosition = 0f;
-		}
-		
-	}
+
 
 	void Update () 
 	{
@@ -112,11 +103,8 @@ public class MenuController : MonoBehaviour
 			if (currentButton > maxButtons - 1) currentButton = 0;
 		}
 
-		location.x = xPosition *  cameraSize;
-
-		location.y = buttons[currentButton].gameObject.transform.position.y;
-		gameObject.transform.position = location;
-
-
+		selectorLocation.x = START_LOCATION_X *  cameraSize;
+		selectorLocation.y = (buttons[currentButton].gameObject.transform.position.y);
+		gameObject.transform.position = selectorLocation;
 	}
 }
